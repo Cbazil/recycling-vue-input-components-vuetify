1cff13269d5aec067ce72b38f3b5f33bdf8a4269
Cbazil <chadbosch08@gmail.com>
2021-01-24 23:13:25 +0200

    cleaned dir

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="home">
+        <h2>Our Home</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
